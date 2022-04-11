a854ee67c3a268ca44dbd1d5e6c56217634deb80
beckjin <1798410296@qq.com>
2022-04-11 14:04:48 +0800

    Update Signature.py

--- Signature.py
+++ Signature.py
@@ -3,11 +3,12 @@ import base64
 
 # 获取签名 timestamp UTC时间戳（精度为毫秒）
 def getSignature(appkey, appsecret, timestamp):
-    dict = {"AppKey": appkey, "SecretKey": appsecret, "Timestamp": timestamp}
-    sorted(dict.items(), key=lambda dict: dict[0], reverse=False)
-    signstr = ""
-    for key in dict:
-        signstr = signstr+'&'+key+'='+dict[key]
+    dict = {"AppKey": appkey, "SecretKey": appsecret,
+            "Timestamp": str(timestamp)}
+    dict_sort = sorted(dict.items(), key=lambda k: k[0], reverse=False)
+    signstr = ''
+    for key, value in dict_sort:
+        signstr = signstr + '&' + key + '=' + value
     signstr = signstr[1:]
     sha = hashlib.sha256()
     sha.update(signstr.encode("utf-8"))
